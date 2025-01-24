8d58dc7b97067fd5fae273f96b85e9c5cee76b70
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2025-01-24 20:27:24 +0200

    add contact page
    - add new colors
    - add links to social media
    - add new text
    - add design for contact page

--- my-portfolio/src/pages/contact/contact.tsx
+++ my-portfolio/src/pages/contact/contact.tsx
@@ -1,11 +1,39 @@
-
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+import classes from './contact.module.css';
+import { Dictionary } from "../../dictionaries/en";
+import { Text } from "@mantine/core";
 
 const Contact = () => {
-   return (
-     <div>
-        
-     </div>
-   );
+  return (
+    <div className={classes.contactWrapper}>
+      <Text className={classes.title}>{Dictionary.contact}</Text>
+      <Text className={classes.text}>{Dictionary.contactText}</Text>
+      <div className={classes.contactLinks}>
+        <a
+          href="https://www.linkedin.com/in/raluca-david-996735242/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.linkedin}
+        >
+          <FaLinkedin /> {Dictionary.linkedIn}
+        </a>
+        <a
+          href="https://github.com/RalucaDavid"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.github}
+        >
+          <FaGithub /> {Dictionary.gitHub}
+        </a>
+        <a
+          href="mailto:andreearalucadavid@gmail.com"
+          className={classes.email}
+        >
+          <FaEnvelope /> {Dictionary.email}
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
