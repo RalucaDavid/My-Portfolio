c2ccc3c42e478fc4e463e4321d2ceeb8ed75d2ef
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2025-01-29 23:37:09 +0200

    change the design from about page and fix bugs related to mobile responsive

--- my-portfolio/src/data/skills.ts
+++ my-portfolio/src/data/skills.ts
@@ -1,25 +1,15 @@
 import { Skill } from "../types/skill";
 
-import typescriptLogo from "../assets/logos/typescript-logo.png";
-import javascriptLogo from "../assets/logos/javascript-logo.png";
-import htmlLogo from "../assets/logos/html-logo.png";
-import cssLogo from "../assets/logos/css-logo.png";
 import reactLogo from "../assets/logos/react-logo.png";
 import angularLogo from "../assets/logos/angular-logo.png";
 import cplusplusLogo from "../assets/logos/cplusplus-logo.png";
 import csharpLogo from "../assets/logos/csharp-logo.png";
-import aspnetLogo from "../assets/logos/aspnet-logo.png";
 import signalrLogo from "../assets/logos/signalr-logo.png";
 
 export const skills: Skill[] = [
     { src: reactLogo, alt: "React logo" },
-    { src: cssLogo, alt: "CSS logo" },
-    { src: htmlLogo, alt: "HTML logo" },
-    { src: typescriptLogo, alt: "TypeScript logo" },
-    { src: javascriptLogo, alt: "JavaScript logo" },
-    { src: signalrLogo, alt: "SignalR logo" },
     { src: cplusplusLogo, alt: "C++ logo" },
     { src: angularLogo, alt: "Angular logo" },
     { src: csharpLogo, alt: "C# logo" },
-    { src: aspnetLogo, alt: "ASP.NET logo" },
+    { src: signalrLogo, alt: "SignalR logo" },
 ];
